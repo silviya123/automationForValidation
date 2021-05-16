bee21abde0821f3592192d49ed85d693bae73eb9
Smilu <smiluthomas88@gmail.com>
2021-05-16 16:47:35 +1000

    Updated Implicit wait

--- test/specs/audio.player.specs.js
+++ test/specs/audio.player.specs.js
@@ -11,29 +11,29 @@ describe('Verify the SBS audio player', () => {
 
 
   it('Verify Subscribe dropdown displays apple and google podcasts', () => {
-    browser.setTimeout({ 'implicit': 20000 });
+    browser.setTimeout({ 'implicit': 15000 });
     vaccineAudioPage.verifySubscribeDropdown();
   });
 
 
   it('verify audio player is launched at the bottom of the screen', () => {
-    browser.setTimeout({ 'implicit': 5000 });
+    browser.setTimeout({ 'implicit': 15000 });
     vaccineAudioPage.verifyAudioPlayerLaunched();
   });
 
 
   it('Click and verify player controls – Play and pause, mute and unmute', () => {
-    browser.setTimeout({ 'implicit': 5000 });
+    browser.setTimeout({ 'implicit': 15000 });
     vaccineAudioPage.verifyPlayerControlsInAudioPlayer();
   });
 
   it('Click 20s forward or rewind on the audio player and verify scrub on the progress bar', () => {
-    browser.setTimeout({ 'implicit': 5000 });
+    browser.setTimeout({ 'implicit': 15000 });
     vaccineAudioPage.verifyFastForwardAndScrub();
   });
 
   it('Verify clicking on language toggle (top right corner of the page) displays language list', () => {
-    browser.setTimeout({ 'implicit': 5000 });
+    browser.setTimeout({ 'implicit': 15000 });
     vaccineAudioPage.verifylanguageToggle();
   });
 
